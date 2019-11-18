5861795bb1099f4b2cd14b3e957e6fb4cc684c5d
yuhostin <yuhostin@gmail.com>
2019-11-18 22:28:32 +0300

    comitetedas
    dcsacxzvfsfwe

---
+++ farm.c
@@ -0,0 +1,151 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_115()
+{
+    return 3267856712U;
+}
+
+unsigned addval_135(unsigned x)
+{
+    return x + 2447018312U;
+}
+
+void setval_364(unsigned *p)
+{
+    *p = 3352987976U;
+}
+
+unsigned addval_448(unsigned x)
+{
+    return x + 2438677106U;
+}
+
+unsigned getval_196()
+{
+    return 2424002281U;
+}
+
+unsigned addval_291(unsigned x)
+{
+    return x + 3285748056U;
+}
+
+unsigned addval_435(unsigned x)
+{
+    return x + 3285748040U;
+}
+
+unsigned getval_274()
+{
+    return 3515435016U;
+}
+
+unsigned getval_224()
+{
+    return 2429651272U;
+}
+
+unsigned getval_100()
+{
+    return 3632875527U;
+}
+
+unsigned addval_185(unsigned x)
+{
+    return x + 3284283720U;
+}
+
+unsigned addval_236(unsigned x)
+{
+    return x + 2446887240U;
+}
+
+void setval_101(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+unsigned addval_393(unsigned x)
+{
+    return x + 2430241096U;
+}
+
+unsigned getval_168()
+{
+    return 2428645704U;
+}
+
+unsigned getval_363()
+{
+    return 3285092680U;
+}
+
+unsigned addval_350(unsigned x)
+{
+    return x + 3666430118U;
+}
+
+unsigned getval_441()
+{
+    return 348347137U;
+}
+
+unsigned addval_106(unsigned x)
+{
+    return x + 3465103930U;
+}
+
+void setval_432(unsigned *p)
+{
+    *p = 3285289032U;
+}
+
+unsigned getval_372()
+{
+    return 3250729288U;
+}
+
+unsigned addval_483(unsigned x)
+{
+    return x + 3032715355U;
+}
+
+void setval_140(unsigned *p)
+{
+    *p = 3515443444U;
+}
+
+unsigned addval_428(unsigned x)
+{
+    return x + 2429454664U;
+}
+
+void setval_150(unsigned *p)
+{
+    *p = 3284633928U;
+}
+
+void setval_472(unsigned *p)
+{
+    *p = 3257944206U;
+}
+
+unsigned addval_469(unsigned x)
+{
+    return x + 2496104776U;
+}
+
+void setval_452(unsigned *p)
+{
+    *p = 3285090632U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
